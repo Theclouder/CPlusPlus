f086aba5f204aa8c82ba65f33dbd70a93df11d8f
Valerio Duchi <vduchi@cbr2s6.42barcelona.com>
2024-01-15 19:49:19 +0100

    Script creating good classes

--- createClass.sh
+++ createClass.sh
@@ -12,21 +12,46 @@ then
 	exit
 fi
 
+# ---	Hpp constructors	 --- #
 header="#include <iostream>
 
 class $1
 {
 "
-first_part_const="
+first_part_hpp="
 	public:
 		$1();
-		$1(const $1 &);"
-second_part_const="
-		$1& operator=(const $1 &);
+		$1(const $1 &);
+"
+second_part_hpp=$'\t\t'
+second_part_hpp+="$1& operator=(const $1 &);
 		~$1();
+"
+
+last_part_hpp="
 };
 "
 
+# ---	Cpp constructors	 --- #
+cpp="#include \"$1.hpp\"
+"
+first_part_cpp="
+$1::$1()"
+
+second_part_cpp="
+$1::$1(const $1 & rhs)"
+
+third_part_cpp="
+{
+	*this = rhs;
+}
+
+$1& $1::operator=(const $1 & rhs)"
+last_part_cpp="
+$1::~$1() {}
+"
+
+# ---	Global vars	 --- #
 idx=0
 keep=0
 class=$1
@@ -36,7 +61,165 @@ arr_err=( "Enter 1/y for yes or 0/n for no" "Enter 1 for protected or 0 for priv
 declare -a all_vars
 declare -a list_vars
 
-create_custom_constr() {
+default_cpp() {
+	if [ ${#all_vars[@]} -ne 0 ]; then
+		for ((i = 0; i <= idx; i++)); do
+			arr=(${all_vars[$i]})
+			if [ ${arr[3]} = "0" ]; then
+				continue;
+			else
+				first_part_cpp+=" : "
+				second_part_cpp+=" : "
+				break;
+			fi
+		done
+		check=0
+		for ((i = 0; i <= idx; i++)); do
+			arr=(${all_vars[$i]})
+			if [ ${arr[3]} = "0" ]; then
+				continue;
+			fi
+			if [ $check -eq 1 ]; then
+				first_part_cpp+=", "
+				second_part_cpp+=", "
+			fi
+			check=1
+			if [ ${arr[2]} = "1" ]; then
+				first_part_cpp+="_${arr[1]}(nullptr)"
+			elif [ ${arr[0]} = "std::string" ]; then
+				first_part_cpp+="_${arr[1]}(\"None\")"
+			elif [ ${arr[0]} = "int" ]; then
+				first_part_cpp+="_${arr[1]}(0)"
+			else
+				first_part_cpp+="_${arr[1]}(NULL)"
+			fi
+			second_part_cpp+="_${arr[1]}(rhs._${arr[1]})"
+		done
+	fi
+	first_part_cpp+=$' {}\n'
+}
+
+operator_cpp() {
+	if [ ${#all_vars[@]} -ne 0 ]; then
+		equal_op+=$'\n{'
+		for ((i = 0; i <= idx; i++)); do
+			arr=(${all_vars[$i]})
+			if [ ${arr[3]} = "1" ]; then
+				continue;
+			fi
+			equal_op+=$'\n\t'
+			equal_op+="this->_${arr[1]} = rhs._${arr[1]};"
+		done
+		equal_op+=$'\n\treturn *this;'
+		equal_op+=$'\n}\n'
+	else
+		equal_op+=$' {}'
+	fi
+}
+
+get_set_cpp() {
+	if [ ${#all_vars[@]} -ne 0 ]; then
+		for ((i = 0; i <= idx; i++)); do
+			arr=(${all_vars[$i]})
+			tmp=$(echo "${arr[1]:0:1}" | tr a-z A-Z)${arr[1]:1}
+			get_set_cpp+=$'\n'
+			if [ ${arr[3]} = "1" ]; then
+				get_set_cpp+="const "
+			fi
+			get_set_cpp+="${arr[0]} $class::get${tmp}() const"
+			get_set_cpp+=$'\n{\n\t'
+			get_set_cpp+="return this->_${arr[1]};"
+			get_set_cpp+=$'\n}\n'
+		done
+		for ((i = 0; i <= idx; i++)); do
+			arr=(${all_vars[$i]})
+			tmp=$(echo "${arr[1]:0:1}" | tr a-z A-Z)${arr[1]:1}
+			if [ ${arr[3]} = "1" ]; then
+				continue;
+			fi
+			get_set_cpp+=$'\n'
+			get_set_cpp+="void $class::set${tmp}(${arr[0]} "
+			if [ ${arr[2]} = "1" ]; then
+				get_set_cpp+=$'* '
+			else
+				get_set_cpp+=$'& '
+			fi
+			get_set_cpp+="${arr[1]})"
+			get_set_cpp+=$'\n{\n\t'
+			get_set_cpp+="this->_${arr[1]} = ${arr[1]};"
+			get_set_cpp+=$'\n}\n'
+		done
+	fi
+}
+
+get_set_hpp() {
+	if [ ${#all_vars[@]} -ne 0 ]; then
+		for ((i = 0; i <= idx; i++)); do
+			get_set_hpp+=$'\n\t\t'
+			arr=(${all_vars[$i]})
+			if [ ${arr[3]} = "1" ]; then
+				get_set_hpp+=$'const '
+			fi
+			get_set_hpp+="${arr[0]}"
+			if [ ${arr[2]} = "1" ]; then
+				get_set_hpp+=$'* '
+			else
+				get_set_hpp+=$' '
+			fi
+			tmp=$(echo "${arr[1]:0:1}" | tr a-z A-Z)${arr[1]:1}
+			get_set_hpp+="get${tmp}() const;"
+		done
+		for ((i = 0; i <= idx; i++)); do
+			get_set_hpp+=$'\n\t\t'
+			arr=(${all_vars[$i]})
+			if [ ${arr[3]} = "1" ]; then
+				continue;
+			fi
+			get_set_hpp+="void "
+			tmp=$(echo "${arr[1]:0:1}" | tr a-z A-Z)${arr[1]:1}
+			get_set_hpp+="set${tmp}("
+			get_set_hpp+="${arr[0]}"
+			if [ ${arr[2]} = "1" ]; then
+				get_set_hpp+=$' *'
+			else
+				get_set_hpp+=$' &'
+			fi
+			get_set_hpp+=");"
+		done
+	fi
+}
+
+custom_constr_cpp() {
+	if [ ${#all_vars[@]} -ne 0 ]; then
+		custom_constr_cpp=$'\n'
+		custom_constr_cpp+="$class::$class("
+		for ((i = 0; i <= idx; i++)); do
+			arr=(${all_vars[$i]})
+			custom_constr_cpp+=$'const '
+			custom_constr_cpp+="${arr[0]} "
+			if [ ${arr[2]} = "1" ]; then
+				custom_constr_cpp+=$'* '
+			fi
+			custom_constr_cpp+="${arr[1]}"
+			if [ $i -ne $idx ]; then
+				custom_constr_cpp+=", "
+			fi
+		done
+		custom_constr_cpp+=") : \\"
+		custom_constr_cpp+=$'\n\t'
+		for ((i = 0; i <= idx; i++)); do
+			arr=(${all_vars[$i]})
+			custom_constr_cpp+="_${arr[1]}(${arr[1]})"
+			if [ $i -ne $idx ]; then
+				custom_constr_cpp+=", "
+			fi
+		done
+		custom_constr_cpp+=$' {}'
+	fi
+	custom_constr_cpp+=$'\n'
+}
+
+custom_constr_hpp() {
 	if [ ${#all_vars[@]} -ne 0 ]; then
 		custom_constr_hpp=$'\t\t'
 		custom_constr_hpp+="$class("
@@ -55,7 +238,7 @@ create_custom_constr() {
 		done
 		custom_constr_hpp+=$');'
 	fi
-	echo $custom_constr_hpp
+	custom_constr_hpp+=$'\n'
 }
 
 print_vars() {
@@ -91,7 +274,7 @@ print_vars() {
 add_variables() {
 	local prv=0
 	local prt=0
-	export IFS=","
+	export IFS="@"
 	if [ ${#all_vars[@]} -eq 0 ]; then
 		return 0
 	fi
@@ -159,26 +342,31 @@ check_value() {
 }
 
 refill_array() {
-	if [ $keep -eq 4 ]; then
-		res=""
-		for i in {0..4}; do
-			res+="${arr_val[$i]},"
-		done
-		all_vars[$idx]=$res
-		echo -e -n "${purple}Do you want to add another property? [Y/n] "
-		read check
-		echo -e -n $reset
-		if [ -z $check ] || [ $check = "Y" -o $check = "y" ]; then
-			keep=0
-			((idx++))
-			return 1
-		elif [ $check != "N" -a $check != "n" ]; then
-			echo -e "$red${arr_err[0]}"
-			refill_array
+	local loop=true
+	while $loop; do
+		if [ $keep -eq 4 ]; then
+			res=""
+			for i in {0..4}; do
+				res+="${arr_val[$i]}@"
+			done
+			all_vars[$idx]=$res
+			echo -e -n "${purple}Do you want to add another property? [Y/n] "
+			read check
+			echo -e -n $reset
+			if [ -z $check ] || [ $check = "Y" -o $check = "y" ]; then
+				keep=0
+				((idx++))
+				return 1
+			elif [ $check = "N" -o $check = "n" ]; then
+				return 2
+			else
+				echo -e "$red${arr_err[0]}"
+				check=""
+			fi
 		else
-			return 2
+			loop=false
 		fi
-	fi
+	done
 	return 0
 }
 
@@ -225,27 +413,21 @@ fi
 #done
 
 add_variables
-create_custom_constr
-exit
-out="$header$vars$first_part_const$custom_const$second_part_const"
+custom_constr_hpp
+get_set_hpp
+operator_cpp
+custom_constr_cpp
+get_set_cpp
+default_cpp
+out_hpp="$header$vars$first_part_hpp$custom_constr_hpp$second_part_hpp$get_set_hpp$last_part_hpp"
+out_cpp="$cpp$first_part_cpp$second_part_cpp$third_part_cpp$equal_op$custom_constr_cpp$last_part_cpp$get_set_cpp"
+#echo "OUT HPP: $out_hpp"
+#echo "OUT CPP: $out_cpp"
+#exit
 cat > $1.hpp << EOF
-$out
+$out_hpp
 EOF
 cat > $1.cpp << EOF
-#include "$1.hpp"
-
-$1::$1() {}
-
-$1::$1(const $1 & rhs)
-{
-	*this = rhs;
-}
-
-$1& $1::operator=(const $1 & rhs)
-{
-
-}
-
-$1::~$1() {}
+$out_cpp
 EOF
 echo -e "${green}Class $1 created!"
